361dee5b0044f100c8d2a383e557ba9843702c91
yop-dev <desilvajoner99@gmail.com>
2025-09-25 20:43:13 +0800

    Configure CORS for Vercel frontend deployment\n\n- Enable CORS to allow requests from Vercel frontend\n- Add FRONTEND_URL environment variable to render.yaml\n- Support both development and production origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,19 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    # Allow requests from Vercel frontend and localhost for development
+    origins [
+      ENV['FRONTEND_URL'],
+      'http://localhost:5173',  # Vite dev server
+      'http://localhost:3000',  # Alternative local port
+      'https://rails-pos-frontend-6teaa6arq-yop-devs-projects.vercel.app' # Your Vercel URL
+    ].compact
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
+  end
+end
