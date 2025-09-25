e1c685fb385e2a01d560015ab7dfb2856221b236
yop-dev <desilvajoner99@gmail.com>
2025-09-25 21:22:36 +0800

    final final fix

--- app/graphql/rails_pos_schema.rb
+++ app/graphql/rails_pos_schema.rb
@@ -1,4 +1,8 @@
 class RailsPosSchema < GraphQL::Schema
-  query(Types::QueryType)
-  mutation(Types::MutationType)
+  # Prevent redefinition errors in development mode
+  unless @_schema_configured
+    query(Types::QueryType)
+    mutation(Types::MutationType)
+    @_schema_configured = true
+  end
 end
