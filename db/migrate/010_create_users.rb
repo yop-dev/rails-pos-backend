22cd06dad9f6350242043d87f831e4ff2be95dd8
yop-dev <desilvajoner99@gmail.com>
2025-09-25 21:12:36 +0800

    Fix users table migration to handle existing table\n\n- Add table_exists? check before creating users table\n- Add index_exists? check before creating email index\n- This prevents PG::DuplicateTable error on Render deployment\n- Migration will now be idempotent and safe to run multiple times

--- db/migrate/010_create_users.rb
+++ db/migrate/010_create_users.rb
@@ -1,15 +1,17 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users do |t|
-      t.string :firstName, null: false
-      t.string :lastName, null: false
-      t.string :email, null: false
-      t.string :password_digest, null: false
-      t.string :role, null: false, default: 'staff'
+    unless table_exists?(:users)
+      create_table :users do |t|
+        t.string :firstName, null: false
+        t.string :lastName, null: false
+        t.string :email, null: false
+        t.string :password_digest, null: false
+        t.string :role, null: false, default: 'staff'
+        
+        t.timestamps
+      end
       
-      t.timestamps
+      add_index :users, :email, unique: true unless index_exists?(:users, :email)
     end
-    
-    add_index :users, :email, unique: true
   end
 end
